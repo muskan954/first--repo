1f99cfb7ed8e2a69057f092d7d1033949c8976a7
muskan <mushuu9234@gmail.com>
2026-06-09 06:01:57 +0530

    Added Types  of  operator in python with example

---
+++ types of operator.py
@@ -0,0 +1,49 @@
+#arithematic operators
+a = 10
+b = 5
+
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division
+print(a % b)  # Modulus
+print(a ** b) # Exponentiation
+
+#comparison operators
+a  = 100
+b = 50
+print(a == b)  # Equal to
+print(a != b)  # Not equal to
+print(a > b)   # Greater than
+print(a < b)   # Less than
+print(a >= b)  # Greater than or equal to
+print(a <= b)  # Less than or equal to
+
+#assignment operators
+num = 10
+num += 5  
+print("num =", num)  # num = 15
+num -= 3
+print("num =", num)  # num = 12
+num *= 2
+print("num =", num)  # num = 24
+num /= 4
+print("num =", num)  # num = 6.0
+num %= 5
+print("num =", num)  # num = 1.0
+num **= 3
+print("num =", num)  # num = 1.0
+ 
+ # logical operators
+print(not False)
+print(not True)
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(True or True)
+
+val1 =True
+val2 =False
+print("OR operator:", val1 or val2) 
+print("AND operator:", val1 and val2)
