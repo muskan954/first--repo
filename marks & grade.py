d83a1c60e6dde39100f3af2de68373050a779d51
muskan <mushuu9234@gmail.com>
2026-06-11 21:57:17 +0530

    its a amazing code

---
+++ marks & grade.py
@@ -0,0 +1,11 @@
+marks = int(input("Enter the marks of the student: "))
+
+if(marks >= 90):
+    print("Grade: A")
+elif(marks >= 80):
+    print("Grade: B")
+elif(marks >= 70):
+    print("Grade: C")
+else :
+    grade = "D"
+    print("Grade of the student ->", grade)
